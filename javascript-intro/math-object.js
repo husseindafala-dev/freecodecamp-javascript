cdd7e0d86cf911dce1bf4f2af78bef7421da8979
husseindafala-dev <meechadama20@gmail.com>
2026-05-21 16:50:11 +0300

    feat: added a new file

---
+++ javascript-intro/math-object.js
@@ -0,0 +1,11 @@
+const largestNumber = Math.max(18, 7, 42, 3, 29);
+console.log(largestNumber);
+const difference = Math.abs(-17.5 - 9.3);
+console.log(difference);
+const a = 3;
+const b = 4;
+const c = Math.hypot(3, 4);
+console.log(c);
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+console.log(randomNumber);
+	
