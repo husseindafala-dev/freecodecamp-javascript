77398958dc74d180c728467543b1f9b1f996652d
husseindafala-dev <meechadama20@gmail.com>
2026-06-03 15:42:13 +0300

    feat: added a new file

---
+++ functions/named-functions/_variable_function.js
@@ -0,0 +1,8 @@
+function getGreeting(name) {
+	let message = "Hello, " + name;
+	return message;
+} 
+
+
+
+console.log(getGreeting("Samuel"));
---
+++ functions/named-functions/return_statement.js
@@ -0,0 +1,6 @@
+function addSum(a, b) {
+	return a + b;
+}
+
+console.log(addSum(3, 7);
+console.log(addSum(10, 8);
