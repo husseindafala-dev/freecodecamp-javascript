3ec494567e5efad5ab9dc750bc298905e9a1dc85
husseindafala-dev <meechadama20@gmail.com>
2026-06-08 15:09:33 +0300

    feat: added new files

---
+++ functions/annonymous-fuction/annonymous-example1.js
@@ -0,0 +1,10 @@
+const calculate = function(a = 2, b=3) {
+	return a * b;
+};
+
+console.log(calculate()); // 6
+console.log(calculate(4)); // 12
+console.log(calculate(4, 5)); // 20
+
+
+// ===
---
+++ functions/annonymous-fuction/annonymus-introduction.js
@@ -0,0 +1,11 @@
+// An anonymous function is a function without a name that can be assigned to a variable.
+
+
+const sum = function(sum1, sum2) {
+	return (sum1 + sum2);
+}
+
+console.log(sum(7, 4));
+
+
+// The function is stored in a variable and called using that variable:
