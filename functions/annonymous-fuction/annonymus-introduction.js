6130b74e703533bb80741faef9d66c1aeb6eb571
husseindafala-dev <meechadama20@gmail.com>
2026-06-09 15:15:13 +0300

    feat: added new file

--- functions/annonymous-fuction/annonymus-introduction.js
+++ functions/annonymous-fuction/annonymus-introduction.js
@@ -7,5 +7,17 @@ const sum = function(sum1, sum2) {
 
 console.log(sum(7, 4));
 
-
 // The function is stored in a variable and called using that variable:
+
+
+
+
+
+
+// Anonymous functions are often used inside other functions.
+// Example: inside setTimeout():
+
+
+setTimeout(function() {
+	console.log("Executed after 2 seconds");
+}, 2000);
