980b2e53fd1fce06a8a349b3e879e3cee5bab9ba
husseindafala-dev <meechadama20@gmail.com>
2026-07-15 15:09:46 +0300

    feat: added a break-statement file

---
+++ js-loops/arrayPush.js
@@ -0,0 +1,22 @@
+const myArray = [];
+
+ for (let i = 1; i <= 10; i++){
+        myArray.push(i);
+ }
+
+console.log(myArray);
+
+
+
+
+
+
+
+
+
+const oddArray = [];
+
+for (let i = 1; i <= 9; i++2){
+	oddArray.push(i);
+}
+console.log(oddArray);
---
+++ js-loops/numbers.js
@@ -0,0 +1,3 @@
+for(let i = 100; 1 >= 1; i--) {
+	console.log(i);
+}
